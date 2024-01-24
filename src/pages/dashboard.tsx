1879c113361232954a2e6fc976fe98275462596d
unknown <amanullahsayeem@gmail.com>
2024-01-24 14:34:36 +0600

    build

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,29 +1,11 @@
-import {
-  mdiAccountMultiple,
-  mdiCartOutline,
-  mdiChartPie,
-  mdiChartTimelineVariant,
-  mdiMonitorCellphone,
-  mdiReload,
-} from '@mdi/js'
+import { mdiChartTimelineVariant } from '@mdi/js'
 import Head from 'next/head'
 import React from 'react'
 import type { ReactElement } from 'react'
-import Button from '../components/Button'
 import LayoutAuthenticated from '../layouts/Authenticated'
 import SectionMain from '../components/Section/Main'
 import SectionTitleLineWithButton from '../components/Section/TitleLineWithButton'
-import CardBoxWidget from '../components/CardBox/Widget'
-import { useSampleClients, useSampleTransactions } from '../hooks/sampleData'
-import CardBoxTransaction from '../components/CardBox/Transaction'
-import { Client, Transaction } from '../interfaces'
-import CardBoxClient from '../components/CardBox/Client'
-import SectionBannerStarOnGitHub from '../components/Section/Banner/StarOnGitHub'
 import CardBox from '../components/CardBox'
-import { sampleChartData } from '../components/ChartLineSample/config'
-import ChartLineSample from '../components/ChartLineSample'
-import NotificationBar from '../components/NotificationBar'
-import TableSampleClients from '../components/Table/UsersTable'
 import { getPageTitle } from '../config'
 import TransactionLogsTable from '../components/Table/TransactionLogsTable'
 import DashboardSection from '../components/DashboardSection'
